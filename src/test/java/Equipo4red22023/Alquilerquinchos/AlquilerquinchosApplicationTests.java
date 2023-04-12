e941c1a98aa79005043d70ea3d29291d4c761f31
JuaniDCR <juanidcr@gmail.com>
2023-04-12 20:22:22 -0300

    Primer Commit

---
+++ src/test/java/Equipo4red22023/Alquilerquinchos/AlquilerquinchosApplicationTests.java
@@ -0,0 +1,13 @@
+package Equipo4red22023.Alquilerquinchos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilerquinchosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
