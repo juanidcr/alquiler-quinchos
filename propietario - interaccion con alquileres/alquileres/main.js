719be6eaa0675b7650ab665c85059c6acc3b11d6
Marina Araceli López <saltara16@gmail.com>
2023-04-24 21:35:27 -0300

    Actualizacion maqueta login

---
+++ propietario - interaccion con alquileres/alquileres/main.js
@@ -0,0 +1,26 @@
+function guardarAlmacenamientoLocal(llave, valor_a_guardar) {
+    localStorage.setItem(llave, JSON.stringify(valor_a_guardar))
+}
+function obtenerAlmacenamientoLocal(llave) {
+    const datos = JSON.parse(localStorage.getItem(llave))
+    return datos
+}
+
+let productos = obtenerAlmacenamientoLocal('productos') || [];
+let contenedor = document.getElementById('contenedor')
+window.addEventListener('load', ()=>{
+    for (let i = 0; i < productos.length; i++){
+        contenedor.innerHTML +=`<div><img src="${productos[i].urlImagen}" alt="producto 1"><div class="informacion"><p>${productos[i].nombre}</p><p class="precio">Precio: $${productos[i].valor}</p><p class="precio">Servicio :${productos[i].servicio}<p><button>Leer más</button></div></div>` 
+    }
+})  
+
+const header = document.querySelector("#header");
+const body = document.querySelector("body");
+window.addEventListener("scroll", function(){
+    if(contenedor.getBoundingClientRect().top<10){
+        header.classList.add("scroll")
+    }
+    else{
+        header.classList.remove("scroll")
+    }
+})
